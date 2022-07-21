3cbba6706ff3329cc5c7df3ca1110384de40e7b6
nasaa <nasaa@Nests-Mac-mini.local>
2022-07-21 20:52:19 +0800

    front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import hero from "./gif/hero.gif"
+import Home from './pages/home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import CreateRoom from './pages/createRoom';
+import JoinRoom from './pages/joinRoom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App '>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="/createRoom" element={<CreateRoom />} />
+            <Route path="/joinRoom" element={<JoinRoom />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
